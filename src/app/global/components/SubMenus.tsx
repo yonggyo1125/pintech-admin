f75dcf6b269418847fe0d1f71bd2637d290f5936
yonggyo1125 <yonggyo00@kakao.com>
2025-02-10 12:22:08 +0900

    .

--- src/app/global/components/SubMenus.tsx
+++ src/app/global/components/SubMenus.tsx
@@ -1,8 +1,38 @@
 'use client'
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import CommonContext from '../contexts/CommonContext'
+import getMenus from '../datas/menus'
+import styled from 'styled-components'
+import classNames from 'classnames'
+
+const StyledMenu = styled.nav``
 
 const SubMenus = () => {
-  return <></>
+  const [menus, setMenus] = useState()
+  const {
+    state: { menuCode, subMenuCode },
+  } = useContext(CommonContext)
+
+  useEffect(() => {
+    setMenus(() => getMenus(menuCode))
+  }, [menuCode])
+
+  return (
+    menus &&
+    menus.length > 0 && (
+      <StyledMenu>
+        {menus.map(({ code, name, url }) => (
+          <a
+            key={'submenu_' + url}
+            href={url}
+            className={classNames('menu', { on: code === subMenuCode })}
+          >
+            {name}
+          </a>
+        ))}
+      </StyledMenu>
+    )
+  )
 }
 
 export default React.memo(SubMenus)
